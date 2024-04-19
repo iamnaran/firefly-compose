a9b30c02934ff313eac0f4a3dd67389044e37502
NaRan <narayanpanthi@MacBook-Air.local>
2024-04-19 10:13:15 +0545

    minor fixes

--- app/src/main/java/com/iamnaran/firefly/data/remote/service/ProductApi.kt
+++ app/src/main/java/com/iamnaran/firefly/data/remote/service/ProductApi.kt
@@ -5,10 +5,14 @@ import com.iamnaran.firefly.domain.dto.ProductResponse
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ProductApi {
     @GET("products")
     suspend fun getAllProducts(): Response<ProductResponse>
+
+    @GET("products")
+    suspend fun getAllProductsByPageSize(@Query("limit") page: Int): Response<ProductResponse>
 
     @GET("products/{id}")
     suspend fun getProductById(@Path("id") productId: String): Response<ProductEntity>
