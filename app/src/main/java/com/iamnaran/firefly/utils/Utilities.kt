489d8db96c472458decbbbb2ec4b7348cc9e9523
NaRan <narayanpanthi@MacBook-Air.local>
2025-02-01 11:01:29 +0545

    minor fixes

--- app/src/main/java/com/iamnaran/firefly/utils/Utilities.kt
+++ app/src/main/java/com/iamnaran/firefly/utils/Utilities.kt
@@ -15,16 +15,6 @@ import com.iamnaran.firefly.R
 
 class Utilities {
 
-//    private fun createShareIntent(activity: Activity, plantName: String) {
-//        val shareText = activity.getString(R.string.share_text_plant, plantName)
-//        val shareIntent = ShareCompat.IntentBuilder(activity)
-//            .setText(shareText)
-//            .setType("text/plain")
-//            .createChooserIntent()
-//            .addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT or Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-//        activity.startActivity(shareIntent)
-//    }
-
     fun openAppSetting(context: Context) {
         val intent = Intent(
             Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.fromParts("package", context.packageName, null)
